c4d62298859c80471c24aad1ce8122b64958ddde
Livnoni <yehuda.livnoni@gmail.com>
2021-11-02 16:30:02 +0200

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -39,6 +39,9 @@ resource "aws_ebs_snapshot" "example_snapshot" {
   }
 }
 
+resource "aws_s3_bucket" "test1" {
+}
+
 resource "aws_security_group" "web-node" {
   # security group is open to the world in SSH port
   name        = "${local.resource_prefix.value}-sg"
